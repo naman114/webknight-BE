bf155672fbe969b27974242f13b085d83adf8893
Naman Gogia <namangogia2001@gmail.com>
2022-10-16 23:55:58 +0530

    feat: add seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,59 @@
-import { PrismaClient } from "@prisma/client";
+import {
+  Prisma,
+  PrismaClient,
+  user,
+  template,
+  certificate,
+} from "@prisma/client";
+import { faker } from "@faker-js/faker";
+
+const getRandomListElement = (items) => {
+  return items[Math.floor(Math.random() * items.length)];
+};
+
+const newTemplate = (): Prisma.templateCreateInput => {
+  return {
+    html: "Hello World",
+    photo: faker.image.avatar(),
+  };
+};
+
+const newUser = (): Prisma.userCreateInput => {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+  };
+};
+
+const newCertificate = (user: user): Prisma.certificateCreateInput => {
+  return {
+    content: faker.lorem.paragraph(),
+    user: { connect: { id: user.id } },
+  };
+};
 
 const prisma = new PrismaClient();
 
 export const seed = async () => {
+  const ROWS = 10;
+
+  const templates: Array<template> = [];
+  const users: Array<user> = [];
+  const certificates: Array<certificate> = [];
+
+  Array.from({ length: ROWS }).forEach(async () => {
+    const template = await prisma.template.create({ data: newTemplate() });
+    templates.push(template);
+
+    const user = await prisma.user.create({ data: newUser() });
+    users.push(user);
+
+    const certificate = await prisma.certificate.create({
+      data: newCertificate(getRandomListElement(users)),
+    });
+    certificates.push(certificate);
+  });
+
   await prisma.$disconnect();
 };
 seed();
